11de051e8fdb1fd7aec9eacfabc880efe734e752
mj-egerton <mjegerton.dev@gmail.com>
2021-07-06 08:56:25 +0100

    initial commit - sass setup

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>Compare your Air</h1>
+    <h2>
+      Compare the air qualities between cities in the UK.
+      <br />
+      Select cities to compare using the search tool below.
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
